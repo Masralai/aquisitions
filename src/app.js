fc7a9477d201b058778c6f615daa29375bb62e8b
Masralai <devdeeppaul2004@gmail.com>
2026-01-19 19:06:25 +0530

    users controllers,routers,services added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from'cors';
 import authRoutes from '#routes/auth.routes.js';
+import userRoutes from '#routes/users.routes.js';
 import securityMiddleware from '#middleware/security.middleware.js';
 
 
@@ -30,5 +31,6 @@ app.get('/api',(req,res)=>{
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 export default app;
